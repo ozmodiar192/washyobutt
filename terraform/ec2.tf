92cc30d972c9b59bc2dd73a3a5189ca38136d668
ozmodiar192 <mattdherrick@gmail.com>
2018-07-20 15:04:48 -0500

    cleaning up, fixing .gitignore

--- terraform/ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "wyb-singleton" {
-  ami             = "ami-2757f631"
-  instance_type   = "t2.nano"
-  key_name        = "wyb.pub"
-  security_groups = ["allow_ssh"]
-  tags {
-    Name = "washyobutt"
-  }
-}
--- terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-# Configure access to AWS as my provider
-provider "aws" {
-  access_key = "${var.accessKey}"
-  secret_key = "${var.secretKey}"
-  region     = "${var.region}"
-}
