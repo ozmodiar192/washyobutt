316a426293ab8bf6583f42c00fd57f1a808ca273
ozmodiar192 <mattdherrick@gmail.com>
2018-07-27 13:57:19 -0500

    Adding terraform provisioner and some hot butt washing content

--- terraform/vpc_public/ec2.tf
+++ terraform/vpc_public/ec2.tf
@@ -2,24 +2,29 @@ resource "aws_instance" "wyb-singleton" {
   ami                         = "ami-2757f631"
   instance_type               = "t2.nano"
   key_name                    = "wyb.pub"
-  security_groups             = ["${aws_security_group.allow_ssh.id}"]
+  security_groups             = ["${aws_security_group.allow_ssh.id}","${aws_security_group.allow_web.id}","${aws_security_group.allow_all_outbound.id}"]
   subnet_id                   = "${aws_subnet.wyb_public_main.id}"
   associate_public_ip_address = true
   tags {
     Name                      = "washyobutt"
   }
-  provisioner "file" {
-    source      = "../../private/wyb_deploy"
-    destination = "/home/ubuntu/.ssh/wyb_deploy"
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = "${file("/home/matt/.ssh/wyb")}"
-    }
-  }
+#  provisioner "file" {
+#    source      = "../../private/wyb_deploy"
+#    destination = "/home/ubuntu/.ssh/wyb_deploy"
+#    connection {
+#      type        = "ssh"
+#      user        = "ubuntu"
+#      private_key = "${file("/home/matt/.ssh/wyb")}"
+#    }
+#  }
   provisioner "remote-exec" {
     inline = [
-      "chmod 600 /home/ubuntu/.ssh/wyb_deploy"
+#      "chmod 600 /home/ubuntu/.ssh/wyb_deploy",
+      "sudo apt-get update",
+      "sudo apt-get -y install nginx",
+      "git clone https://github.com/ozmodiar192/washyobutt.git",
+      "sudo ln -s ~/washyobutt/content/* /var/www/html/",
+      "sudo service nginx start",
     ]
     connection {
       type        = "ssh"
--- terraform/vpc_public/security.tf
+++ terraform/vpc_public/security.tf
@@ -1,7 +1,7 @@
 #Create a security group to allow incoming ssh from my current external IP
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow all inbound traffic"
+  description = "Allow inbound SSH traffic from my ip"
   vpc_id      = "${aws_vpc.wyb_public.id}"
 
   ingress {
@@ -10,6 +10,38 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["${chomp(data.http.icanhazip.body)}/32"]
   }
+
+  tags {
+    Name = "allow_ssh"
+  }
+}
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow website inbound traffic from all IPs"
+  vpc_id      = "${aws_vpc.wyb_public.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443 
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_web"
+  }
+}
+resource "aws_security_group" "allow_all_outbound" {
+  name        = "allow_all_outbound"
+  description = "Allow all outbound traffic"
+  vpc_id      = "${aws_vpc.wyb_public.id}"
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -27,6 +59,3 @@ resource "aws_key_pair" "wyb_public" {
   key_name   = "wyb.pub"
   public_key = "${var.publicKey}"
 }
-
-# Upload github private key for access to deployer
-
